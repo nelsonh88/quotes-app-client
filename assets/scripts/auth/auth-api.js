ecd822c23c8d25c31e7fd0b5db24ad11a8fe0d78
nelsonh88 <nelson.huezo88@gmail.com>
2018-04-14 14:02:32 -0400

    Adds crud on users
    sign-up
    - Added a form in HTML for sign-up
    - Added the eventHandler to index.js
    - Added the ajax request in auth-api.js for sign-up
    - Added the the success and failure for sign-up in auth-ui.js
    - Added the sign-up event in auth-events.js
    
    sign-in
    - Added a form in HTML for sign-in
    - Added the eventHandler to index.js
    - Added the ajax request in auth-api.js for sign-in
    - Added the the success and failure for sign-in in auth-ui.js
    - Added the sign-in event in auth-events.js
    
    change-password
    - Added a form in HTML for change-password
    - Added the eventHandler to index.js
    - Added the ajax request in auth-api.js for change-password
    - Added the the success and failure for change-password in auth-ui.js
    - Added the change-password event in auth-events.js
    
    sign-out
    - Added a form in HTML for sign-out
    - Added the eventHandler to index.js
    - Added the ajax request in auth-api.js for sign-out
    - Added the the success and failure for sign-out in auth-ui.js
    - Added the sign-out event in auth-events.js

---
+++ assets/scripts/auth/auth-api.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json'
+    },
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  console.log('sign-in works')
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json'
+    },
+    data: data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password/',
+    method: 'PATCH',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+const signOut = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out/', // + store.user.id
+    method: 'DELETE',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const authEvents = require('./auth/auth-events.js')
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +9,5 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  authEvents.addHandlers()
 })
